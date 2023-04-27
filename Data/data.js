96a195f333d63f2a01a74f527ade4222718abb6b
Nurmuhammad <nurmuhammad2007qosimov@gmail.com>
2023-04-27 17:59:58 +0500

    Fs api

--- Data/data.js
+++
@@ -1,9 +0,0 @@
-let data = [
-    // {
-    //     id: 2,
-    //     firstName:  'first name',
-    //     lastName:  'last name',
-    //     email:  'anpch@example.com',
-    // },
-]
-module.exports = data
--- Routes/Teacher.js
+++ Routes/Teacher.js
@@ -1,47 +1,84 @@
 const { Router } = require('express')
 const router = Router()
-let data = require('../Data/data')
+let DATA = require('../Data/data')
 const { v4 } = require('uuid')
+const fs = require('fs')
+const path = require('path')
 
 //get All teachers
 
 router.get('/', (req, res) => {
-    res.json({
-        message: 'All Teachers',
-        data: data
+
+    fs.readFile(path.join(__dirname, '../Data/data.json'), 'utf-8', (err, data) => {
+        if (err) throw err
+        else {
+            data = JSON.parse(data)
+            res.json({
+                message: 'All Teachers',
+                data: data
+            })
+        }
     })
 })
 
 //get one teacher
 
 router.get('/:id', (req, res) => {
-    res.json(data.filter(item => item.id == req.params.id))
+    res.json(DATA.filter(item => item.id == req.params.id))
 })
 
 //create ucer
 
 router.post('/', (req, res) => {
-    const body = req.body
-    const newTeacher = {
-        id: v4(),
-        firstName: body.firstName,
-        lastName: body.lastName,
-        email: body.email,
-    }
-    data.push(newTeacher)
-    res.json({
-        message: 'Teacher Created',
-        data: newTeacher
+    fs.readFile(path.join(__dirname, '../Data/data.json'), 'utf-8', (err, data) => {
+        if (err) throw err
+        else {
+            const newData = {
+                id: v4(),
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email
+            }
+            data = JSON.parse(data)
+            data.push(newData)
+            // res.json(data);
+            fs.writeFile(path.join(__dirname, '../Data/data.json'), JSON.stringify(data), (err) => {
+                if (err) throw err
+                else {
+                    res.json({
+                        message: 'Teacher Created',
+                        data: newData
+                    })
+                }
+            })
+        }
     })
 })
 
 //delete
 
 router.delete('/:id', (req, res) => {
-    data = data.filter(item => item.id != req.params.id)
-    res.json({
-        message: 'Teacher Deleted',
-        data: data
+    fs.readFile(path.join(__dirname, '../Data/data.json'), 'utf-8', (err, data) => {
+        if (err) throw err
+        else {
+            data = JSON.parse(data)
+            const isExist = data.some(data => data.id == req.params.id)
+            if (isExist) {
+                data = data.filter(item => item.id != req.params.id)
+                fs.writeFile(path.join(__dirname, '../Data/data.json'), JSON.stringify(data), (err) => {
+                    if (err) throw err
+                    else {
+                        res.json({
+                            message: 'Teacher Deleted',
+                        })
+                    }
+                })
+            } else {
+                res.json({
+                    message: 'Teacher Not Found',
+                })
+            }
+        }
     })
 })
 
@@ -49,20 +86,40 @@ router.delete('/:id', (req, res) => {
 
 router.put('/:id', (req, res) => {
     const body = req.body
-    const isExist = data.some(item => item.id == req.params.id)
-    if(isExist){
-        data.map(item => {
-            if(item.id == req.params.id){
-                item.firstName = body.firstName? body.firstName : item.firstName
-                item.lastName = body.lastName? body.lastName: item.lastName
-                item.email = body.email? body.email : item.email
+    fs.readFile(path.join(__dirname, '../Data/data.json'), 'utf-8', (err, data) => {
+        if (err) throw err
+        else {
+            data = JSON.parse(data)
+            const isExist = data.some(data => data.id == req.params.id)
+            if (isExist) {
+                const index = data.findIndex(item => item.id == req.params.id)
+                const putData = {
+                    id: req.params.id,
+                    firstName: body.firstName ? body.firstName : data[index].firstName,
+                    lastName: body.lastName ? body.lastName : data[index].lastName,
+                    email: body.email ? body.email : data[index].email
+                }
+                data[index] = putData
+                res.json(data)
+    
+                fs.writeFile(path.join(__dirname, '../Data/data.json'), JSON.stringify(data), (err) => {
+                    if (err) throw err
+                    else {
+                        res.json({
+                            message: 'Teacher Updated',
+                            data: putData
+                        })
+                    }
+                })
+            }else{
                 res.json({
-                    message: 'teacher updated',
-                    data: item
+                    message: 'Teacher Not Found',
                 })
             }
-        })
-    }
+
+        }
+    })
+
 })
 
 module.exports = router
